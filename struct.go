0673c187761622f65b01c165bf4fba081a5b5032
xiaojian <huangxj1@fosun.com>
2021-10-07 17:35:05 +0800

    tree

--- struct.go
+++ struct.go
@@ -1,5 +1,10 @@
 package algo
 
+import (
+	"fmt"
+
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -120,4 +125,305 @@ func (q *Queue) Empty() bool {
 func (q *Queue) Size() int {
 
 	return len(q.val)
+}
+
+/** tree **/
+type BinaryTree interface {
+	PrevOrderByIteration() (valList []int, nodeList []*TreeNode)
+	PrevOrderByRecursive() (valList []int, nodeList []*TreeNode)
+
+	InOrderByIteration() (valList []int, nodeList []*TreeNode)
+	InOrderByRecursive() (valList []int, nodeList []*TreeNode)
+
+	NextOrderByIteration() (valList []int, nodeList []*TreeNode)
+	NextOrderByRecursive() (valList []int, nodeList []*TreeNode)
+}
+
+type TreeNode struct {
+	Left  *TreeNode
+	Right *TreeNode
+	Val   int
+}
+
+func GeneralTree(data []int) *TreeNode {
+
+	const empty = -999
+
+	root := &TreeNode{Val: data[0]}
+	var que []*TreeNode
+	que = append(que, root)
+
+	cur := root
+
+	i := 1
+	for cur != nil && len(que) > 0 {
+		//left
+		if i < len(data) && data[i] != empty {
+			cur.Left = &TreeNode{Val: data[i]}
+			que = append(que, cur.Left)
+		}
+		i++
+		//right
+		if i < len(data) && data[i] != empty {
+
+			cur.Right = &TreeNode{Val: data[i]}
+			que = append(que, cur.Right)
+		}
+		i++
+
+		if i > len(data)-1 {
+			break
+		}
+		que = que[1:]
+		cur = que[0]
+	}
+	return root
+}
+
+/**
+层序遍历
+**/
+func (root *TreeNode) LevelOrder() (valList []int, nodeList []*TreeNode, valLevel [][]int) {
+
+	var que []*TreeNode
+
+	que = append(que, root)
+
+	for len(que) > 0 {
+    
+        var curLevel []int
+        var nextQue []*TreeNode
+
+        for i:= 0; i < len(que); i ++ {
+
+            valList = append(valList, que[i].Val)
+            nodeList = append(nodeList, que[i])
+            curLevel = append(curLevel, que[i].Val)
+
+            if que[i].Left != nil {
+                nextQue = append(nextQue, que[i].Left)
+            }
+            if que[i].Right!= nil {
+                nextQue = append(nextQue, que[i].Right)
+            }
+
+        }
+        valLevel = append(valLevel, curLevel)
+        que = nextQue
+	}
+
+	return valList, nodeList, valLevel
+}
+
+/**
+递归前序
+中左右
+**/
+func (root *TreeNode) PrevOrderByRecursive() (valList []int, nodeList []*TreeNode) {
+
+	var sub func(node *TreeNode, valList *[]int, nodeList *[]*TreeNode)
+
+	sub = func(node *TreeNode, valList *[]int, nodeList *[]*TreeNode) {
+
+		*valList = append(*valList, node.Val)
+		*nodeList = append(*nodeList, node)
+
+		if node.Left != nil {
+			sub(node.Left, valList, nodeList)
+		}
+		if node.Right != nil {
+			sub(node.Right, valList, nodeList)
+		}
+	}
+
+	sub(root, &valList, &nodeList)
+	fmt.Println(valList)
+	return valList, nodeList
+}
+
+
+/**
+迭代前序
+中左又
+**/
+
+func (root *TreeNode) PrevOrderByIteration() (valList []int, nodeList []*TreeNode) {
+    var stack []*TreeNode
+
+    stack = append(stack, root)
+
+    for len(stack) > 0 {
+        
+        cur := stack[0]
+        valList = append(valList, cur.Val)
+        nodeList = append(nodeList, cur)
+        
+        if len(stack) > 1 {
+            stack = stack[1:]
+        }else {
+            stack = stack[0:0]
+        }
+
+        if cur.Right != nil{
+
+            stack = append([]*TreeNode{cur.Right}, stack...)
+        }
+        if cur.Left!= nil{
+
+            stack = append([]*TreeNode{cur.Left}, stack...)
+        }
+    }
+
+    return valList, nodeList
+}
+
+/**
+递归中序
+左中右
+**/
+func (root *TreeNode) InOrderByRecursive() (valList []int, nodeList []*TreeNode) {
+
+	var sub func(node *TreeNode, valList *[]int, nodeList *[]*TreeNode)
+
+	sub = func(node *TreeNode, valList *[]int, nodeList *[]*TreeNode) {
+
+		if node.Left != nil {
+			sub(node.Left, valList, nodeList)
+		}
+
+		*valList = append(*valList, node.Val)
+		*nodeList = append(*nodeList, node)
+
+		if node.Right != nil {
+			sub(node.Right, valList, nodeList)
+		}
+	}
+
+	sub(root, &valList, &nodeList)
+	fmt.Println(valList)
+	return valList, nodeList
+}
+
+/*
+迭代中序
+左中右
+*/
+func (root *TreeNode) InOrderByIteration() (valList []int, nodeList []*TreeNode) {
+    
+
+    var stack []*TreeNode
+    
+    var lastPop *TreeNode
+
+    cur := root
+
+    for cur != nil {
+
+        for cur != nil {
+
+            stack = append([]*TreeNode{cur}, stack...)
+            cur = cur.Left
+        }
+
+        for len(stack) > 0 {
+
+            cur = stack[0]
+            //出栈: 左节点为空，左节点已经出栈
+            if (cur.Left == nil ) || (cur.Left != nil && cur.Left == lastPop) {
+
+                valList = append(valList, cur.Val)
+                nodeList = append(nodeList, cur)
+                lastPop = cur
+                if len(stack) > 1 {
+                    stack = stack[1:]
+                }else {
+                    stack = stack[0:0]
+                }
+            }
+
+            //右节点重新迭代
+            if cur.Right != nil{
+                cur = cur.Right
+                break
+
+            }
+            
+        }
+    }
+    return valList, nodeList
+}
+
+
+/**
+递归后序
+左右中
+**/
+func (root *TreeNode) NextOrderByRecursive() (valList []int, nodeList []*TreeNode) {
+
+	var sub func(node *TreeNode, valList *[]int, nodeList *[]*TreeNode)
+
+	sub = func(node *TreeNode, valList *[]int, nodeList *[]*TreeNode) {
+
+		if node.Left != nil {
+			sub(node.Left, valList, nodeList)
+		}
+
+		if node.Right != nil {
+			sub(node.Right, valList, nodeList)
+		}
+
+		*valList = append(*valList, node.Val)
+		*nodeList = append(*nodeList, node)
+	}
+
+	sub(root, &valList, &nodeList)
+	fmt.Println(valList)
+	return valList, nodeList
+}
+
+/**
+迭代后序遍历
+左右中
+**/
+func (root *TreeNode) NextOrderByIteration() (valList []int, nodeList []*TreeNode) {
+    
+    var stack []*TreeNode
+    
+    var lastPop *TreeNode
+
+    cur := root
+
+    for cur != nil {
+
+        for cur != nil {
+
+            stack = append([]*TreeNode{cur}, stack...)
+            cur = cur.Left
+        }
+
+        for len(stack) > 0 {
+
+            cur = stack[0]
+            //出栈: 叶子节点,右边节点已出，无右左出，
+            if (cur.Left == nil && cur.Right == nil ) || (cur.Right != nil && cur.Right == lastPop) || (cur.Right == nil && cur.Left == lastPop) {
+
+                valList = append(valList, cur.Val)
+                nodeList = append(nodeList, cur)
+                lastPop = cur
+                if len(stack) > 1 {
+                    stack = stack[1:]
+                }else {
+                    stack = stack[0:0]
+                }
+            }else if cur.Right != nil{
+                //右节点重新迭代
+                cur = cur.Right
+                break
+
+            }
+            
+        }
+    }
+
+    return valList, nodeList
 }
--- struct_test.go
+++ struct_test.go
@@ -27,20 +27,89 @@ func TestGetListValues(t *testing.T) {
 	assert.Equal(t, []int{1, 2, 3, 4}, values)
 }
 
+func TestStack(t *testing.T) {
+	stack := Stack{}
+	stack.Push(5)
+	stack.Push(4)
+	stack.Push(3)
+	stack.Push(2)
+	stack.Push(1)
 
-func TestStack (t *testing.T) {
-    stack := Stack{}
-    stack.Push(5)
-    stack.Push(4)
-    stack.Push(3)
-    stack.Push(2)
-    stack.Push(1)
+	assert.Equal(t, 1, stack.Pop())
+	assert.Equal(t, 2, stack.Pop())
+	assert.Equal(t, 3, stack.Pop())
+	assert.Equal(t, 4, stack.Pop())
+	assert.Equal(t, 5, stack.Pop())
 
-    assert.Equal(t, 1, stack.Pop())
-    assert.Equal(t, 2, stack.Pop())
-    assert.Equal(t, 3, stack.Pop())
-    assert.Equal(t, 4, stack.Pop())
-    assert.Equal(t, 5, stack.Pop())
+	assert.Equal(t, false, stack.IsEmpty())
+}
 
-    assert.Equal(t, false, stack.IsEmpty())
+func TestGeneralTree(t *testing.T) {
+
+	testSlice := []int{1, 2, 3, 4, 5, 6}
+	root := GeneralTree(testSlice)
+
+    assert.Equal(t, 2, root.Left.Val)
+    assert.Equal(t, 4, root.Left.Left.Val)
+    assert.Equal(t, 5, root.Left.Right.Val)
+
+    assert.Equal(t, 3, root.Right.Val)
+    assert.Equal(t, 6, root.Right.Left.Val)
+
+}
+func TestLevelOrder(t *testing.T) {
+    testSlice := []int{7,6,5, 3, 4, 9,  8}
+	root := GeneralTree(testSlice)
+
+    res, _,_ := root.LevelOrder()
+    assert.Equal(t, []int{7,6,5, 3, 4, 9,  8}, res)
+}
+
+func TestPrevOrderByRecursive(t *testing.T) {
+    testSlice := []int{1, 2, 3, 4, 5, 6}
+	root := GeneralTree(testSlice)
+
+    res, _ := root.PrevOrderByRecursive()
+    assert.Equal(t, []int{1,2,4,5,3,6}, res)
+}
+
+func TestPrevOrderByIteration(t *testing.T) {
+    testSlice := []int{7,6,5, 3, 4, -999, -999, 9, -999, -999, 8}
+	root := GeneralTree(testSlice)
+
+    res, _ := root.PrevOrderByIteration()
+    assert.Equal(t, []int{7,6,3,9,4,8,5}, res)
+}
+
+func TestInOrderByRecursive(t *testing.T) {
+    testSlice := []int{7,6,5, 3, 4, -999, -999, 9, -999, -999, 8}
+	root := GeneralTree(testSlice)
+
+    res, _ := root.InOrderByRecursive()
+    assert.Equal(t, []int{9,3,6,4,8,7,5}, res)
+}
+
+func TestInOrderByIteration(t *testing.T) {
+    testSlice := []int{7,6,5, 3, 4, -999, -999, 9, -999, -999, 8}
+	root := GeneralTree(testSlice)
+
+    res, _ := root.InOrderByIteration()
+    assert.Equal(t, []int{9,3,6,4,8,7,5}, res)
+}
+
+
+func TestNextOrderByRecursive(t *testing.T) {
+    testSlice := []int{7,6,5, 3, 4, -999, -999, 9, -999, -999, 8}
+	root := GeneralTree(testSlice)
+
+    res, _ := root.NextOrderByRecursive()
+    assert.Equal(t, []int{9,3,8,4,6,5,7}, res)
+}
+
+func TestNextOrderByIteration(t *testing.T) {
+    testSlice := []int{7,6,5, 3, 4, -999, -999, 9, -999, -999, 8}
+	root := GeneralTree(testSlice)
+
+    res, _ := root.NextOrderByIteration()
+    assert.Equal(t, []int{9,3,8,4,6,5,7}, res)
 }
