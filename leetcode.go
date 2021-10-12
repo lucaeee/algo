a4ab4e49dc7dbdda2e66f6833dbc4231f8fc6209
xiaojian <huangxj1@fosun.com>
2021-10-12 11:19:38 +0800

    arraytoBst

--- leetcode.go
+++ leetcode.go
@@ -2615,3 +2615,38 @@ func TrimBST(root *TreeNode, low int, high int) *TreeNode {
 
     return root
 }
+
+/**
+108. 将有序数组转换为二叉搜索树
+给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+
+高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+**/
+func SortedArrayToBST(nums []int) *TreeNode {
+
+
+    if len(nums) == 0 {
+
+        return nil
+    }
+
+
+    mid :=len(nums)/2
+
+    left := nums[:mid]
+    right := nums[mid+1:]
+
+    cur := &TreeNode{Val: nums[mid]}
+
+    if len(left) > 0 {
+        
+        cur.Left = SortedArrayToBST(left)
+    }
+
+    if len(right) > 0 {
+        
+        cur.Right = SortedArrayToBST(right)
+    }
+    return cur
+}
+
